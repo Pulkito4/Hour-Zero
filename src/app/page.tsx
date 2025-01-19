d246a29b0ddb9a1792c005d0c21b533a1ca3ec06
Pulkit Goyal <pulkitcgoyal@gmail.com>
2025-01-19 17:18:23 +0530

    minor fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,57 +4,53 @@ import { Offer } from "@/components/Offer";
 import { WavyBackground } from "@/components/ui/wavy-background";
 import { WhyUs } from "@/components/Whyus";
 
-
 export default function Home() {
-  return (
-    <>
-      <div className="w-full h-full font-work-sans text-white">
-        {/* tagline */}
-        <div className="mt-15  font-work-sans text-center font-semibold">
-          <span className="text-4xl  text-white">
-            HOUR <span className="text-purple-400">ZERO</span>
-          </span>
-          <p className="text-gray-400  text-xl !font-normal">
-            Because Every Mark Counts
-          </p>
-        </div>
+	return (
+		<>
+			<div className="w-full h-full font-work-sans text-white">
+				{/* tagline */}
+				<div className="mt-15  font-work-sans text-center font-semibold">
+					<span className="text-4xl  text-white">
+						HOUR <span className="text-purple-400">ZERO</span>
+					</span>
+					<p className="text-gray-400  text-xl !font-normal">
+						Because Every Mark Counts
+					</p>
+				</div>
 
-        <div className="relative h-[500px] flex items-center justify-center overflow-hidden">
-          {/* WavyBackground Container */}
-          <div className="absolute inset-0 w-full h-[500px]">
-            <WavyBackground />
-          </div>
+				<div className="relative h-[500px] flex items-center justify-center overflow-hidden">
+					{/* WavyBackground Container */}
+					<div className="absolute inset-0 w-full h-[500px]">
+						<WavyBackground />
+					</div>
 
-          {/* Dropdown Container */}
-          <div className="relative z-50 w-full max-w-xl mx-auto px-4 ">
-            <Dropdown />
-          </div>
-        </div>
+					{/* Dropdown Container */}
+					<div className="relative z-10 w-full max-w-xl mx-auto px-4 ">
+						<Dropdown />
+					</div>
+				</div>
 
-        <div className="px-8 mt-15  mb-15  ">
-        <h1 className="text-white ml-4 mb-5 text-3xl font-bold text-center">
-            WHAT WE OFFER
-          </h1>
-          <Offer />
-        </div>
+				<div className="px-8 mt-15  mb-15  ">
+					<h1 className="text-white ml-4 mb-5 text-3xl font-bold text-center">
+						WHAT WE OFFER
+					</h1>
+					<Offer />
+				</div>
 
-        <div className="px-8 mt-15  mb-15 ">
-          <h1 className="text-white ml-4 mb-9 text-3xl font-bold">
-            MEET OUR TOP CONTRIBUTORS
-          </h1>
-          <CarousalSlider />
-        </div>
+				<div className="px-8 mt-15  mb-15 ">
+					<h1 className="text-white ml-4 mb-9 text-3xl font-bold text-center">
+						MEET OUR TOP CONTRIBUTORS
+					</h1>
+					<CarousalSlider />
+				</div>
 
-
-        <div className="px-8 mt-15  mb-15 ">
-          <h1 className="text-white ml-4 mb-5 text-3xl font-bold">
-            WHY HOUR ZERO ?
-          </h1>
-        <WhyUs/>
-        </div>
-
-        
-      </div>
-    </>
-  );
+				<div className="px-8 mt-15  mb-15 flex flex-col items-center">
+					<h1 className="text-white ml-4 mb-5 text-3xl font-bold text-center">
+						WHY HOUR ZERO ?
+					</h1>
+					<WhyUs />
+				</div>
+			</div>
+		</>
+	);
 }
--- src/components/Whyus.tsx
+++ src/components/Whyus.tsx
@@ -4,10 +4,10 @@ import { Card } from "flowbite-react";
 export function WhyUs() {
   return (
     <Card href="#" className="max-w-sm">
-      <h5 className="text-2xl font-bold tracking-tight text-gray-900">
+      <h5 className="text-2xl font-bold tracking-tight text-gray-900 text-center">
         Exaggerated explaination of why Hour Zero is the best!
       </h5>
-      <p className="font-normal text-gray-700 ">
+      <p className="font-normal text-gray-700 text-center ">
         Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
       </p>
     </Card>
